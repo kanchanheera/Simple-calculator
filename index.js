8aa216c5d863549bc445dcc69dcc01299c8a9173
Kanchan Heera <161306339+kanchanheera@users.noreply.github.com>
2024-03-25 03:58:15 +0500

    Compiled to js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
     {
         message: "Select one of the operators to perform action",
         type: "list",
@@ -26,3 +26,4 @@ else if (answer.operator === "Division") {
 else {
     console.log("Please select valid operator");
 }
+console.log('THE END');
